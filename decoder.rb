1a0c5cc5e02513be2f69db20800c7f0edca69510
kayjee <kagiso.killagram@gmail.com>
2023-08-03 15:25:08 +0200

    update file

--- decoder.rb
+++ decoder.rb
@@ -2,16 +2,10 @@
 
 # Your other methods and code here...
 
-# Decode an entire word in Morse code.
-# Takes a string parameter `morse_word` and returns the string representation.
-# Every character in a word will be separated by a single space.
 def decode_word(morse_word)
   morse_word.split.map { |char| decode_char(char) }.join
 end
 
-# Decode the entire message in Morse code.
-# Takes a string parameter `morse_message` and returns the string representation.
-# Every word will be separated by 3 spaces.
 def decode(morse_message)
   morse_message.split('   ').map { |word| decode_word(word) }.join(' ')
 end
