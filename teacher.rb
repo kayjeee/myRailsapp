e1fb6b28714fd79919033158d3c740ef235a0c60
kayjee <kagiso.killagram@gmail.com>
2023-08-09 08:30:18 +0200

    fixed linters

--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,12 @@
 require_relative 'person'
 
+# Represents a teacher, inheriting from the Person class.
 class Teacher < Person
-  def initialize(id, age, specialization, parent_permission: true, name: "Unknown")
-    super(id, age, parent_permission: parent_permission, name: name)
+  def initialize(id, age, specialization, parent_permission: true, name: 'Unknown')
+    super(id, age, parent_permission:, name:)
     @specialization = specialization
   end
-  
+
   def can_use_services?
     true
   end
